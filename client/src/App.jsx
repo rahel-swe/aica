d4463c00d47ceae9aad0f81df688eb59ed315305
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2025-09-17 09:03:12 +0430

    Initial commit.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./components/ui/button";
-import { Binary } from "lucide-react";
+import { Binary, User } from "lucide-react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,7 +25,7 @@ function App() {
           className="cursor-pointer"
         >
           count is {count}
-          <Binary className="text-blue-500" />
+          <User className="text-blue-500 " />
         </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
