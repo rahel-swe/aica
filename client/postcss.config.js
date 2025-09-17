3741ecf5cdf554e9f1fa32c1dcf0ce2f4d0261c7
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2025-09-17 07:22:21 +0430

    Configured Tailwind + ShadeCn + Theme Provider

---
+++ client/postcss.config.js
@@ -0,0 +1,11 @@
+export default {
+  plugins: {
+    // No tailwindcss because it handeled by vite
+
+    // "@tailwindcss/postcss": {},
+    autoprefixer: {}, // optional if you want browser-prefix support
+    // other PostCSS plugins you want:
+    // 'postcss-nesting': {},
+    // 'postcss-your-plugin': {}
+  },
+};
---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
