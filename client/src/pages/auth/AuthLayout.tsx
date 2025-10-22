b2b0473b64b98249f05ffa6e29bfe70632a6cfa2
Milad Noori <miladgulnoori@gmail.com>
2025-10-22 10:44:05 +0430

    Implement react-hook-form, zod(form validation)

--- client/src/pages/auth/AuthLayout.tsx
+++ client/src/pages/auth/AuthLayout.tsx
@@ -1,16 +1,16 @@
-import { FieldDescription } from '@/components/ui/field';
-import { Outlet } from 'react-router-dom';
+import { FieldDescription } from '@/components/ui/field'
+import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
   return (
-    <div className="m-4">
+    <div className='m-4'>
       <Outlet />
-      <FieldDescription className="px-6 py-4 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
-        and <a href="#">Privacy Policy</a>.
+      <FieldDescription className='px-6 py-4 text-center'>
+        By clicking continue, you agree to our <a href='#'>Terms of Service</a>{' '}
+        and <a href='#'>Privacy Policy</a>.
       </FieldDescription>
     </div>
-  );
-};
+  )
+}
 
-export default AuthLayout;
+export default AuthLayout
