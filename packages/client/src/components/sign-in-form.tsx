81b3eabf8d2ddf262439f165a2efdd7b1b797848
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2026-01-28 15:43:46 +0430

    Add Sign In form types

---
+++ packages/client/src/components/sign-in-form.tsx
@@ -0,0 +1,9 @@
+import type { SignInFormType } from '@contracts/shared/types/auth-types';
+
+const SignInForm = () => {
+  const onSubmit = (data: SignInFormType) => {};
+
+  return <div> SignInForm</div>;
+};
+
+export default SignInForm;
--- packages/contracts/src/types/auth-types.ts
+++ packages/contracts/src/types/auth-types.ts
@@ -2,3 +2,9 @@ export type SignInFormType = {
   email: string;
   password: string;
 };
+
+export type SignUpFormType = {
+  name: string;
+  email: string;
+  password: string;
+};
