3876ca11231ffed946832964fc9998706454208c
Esmat <aesmatullah20@gmail.com>
2026-02-04 12:39:46 +0430

    feat:(auth) "export useSession" is removed form auth-client

--- packages/client/src/lib/auth-client.ts
+++ packages/client/src/lib/auth-client.ts
@@ -1,9 +1,9 @@
-import { createAuthClient } from 'better-auth/client';
+import { createAuthClient } from "better-auth/client";
 
+// Create auth instance
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api,'
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api,",
 });
 
-
-export { useSession } from 'better-auth/react';
-
+// For react hooks
+// import { createAuth } from "better-auth";
