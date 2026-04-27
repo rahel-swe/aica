a7a5be123c03343776bd90a3d95b29cc0fe3fd4b
Rasooli <shahidullahrasooli35@gmail.com>
2026-04-27 09:38:30 +0430

    refac: change password show icons

--- packages/client/src/components/Password/password-input.tsx
+++ packages/client/src/components/Password/password-input.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
 
 type PasswordInputProps = {
   value: string;
@@ -28,7 +29,7 @@ export default function PasswordInput({
         onClick={() => setShowPassword((prev) => !prev)}
         className="absolute right-4 top-1/2 -translate-y-1/2 text-muted-foreground"
       >
-        {showPassword ? '🙈' : '👁️'}
+        {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
       </button>
     </div>
   );
