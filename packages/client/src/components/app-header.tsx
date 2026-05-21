e4330f15b1a57e1e5aa5cea423a2502c5f16963c
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2026-05-21 20:25:19 +0430

    feat: create app images resources object base on theme

--- packages/client/src/components/app-header.tsx
+++ packages/client/src/components/app-header.tsx
@@ -5,24 +5,26 @@ import { Link } from 'react-router-dom';
 
 import ModeToggle from './toggle-mode';
 import { Button } from './ui/button';
+import { useTheme } from '@/providers/theme-provider';
+import { appImgSources } from '@/constants/app-image-sources';
 
 export default function AppHeader() {
   const { data, isPending } = authClient.useSession();
+  const { theme } = useTheme();
 
   return (
-    <header className="sticky top-1 md:top-2 md:mx-auto z-50 border-x md:border bg-background/20 backdrop-blur-lg supports-backdrop-filter:bg-background/20 mx-1  rounded-full">
-      <div className="mx-auto flex h-16 items-center justify-between gap-3 px-3 sm:px-5">
-        <Link to="/app/dashboard" className="flex shrink-0 items-center gap-2">
-          <div className="flex items-center gap-1">
-            {/* Logo */}
-            <picture>
-              <source
-                srcSet="/dark-logo.png"
-                media="(prefers-color-scheme: dark)"
-              />
+    <div className="sticky top-1 px-1 md:top-2 md:mx-auto md:max-w-lg z-50 w-full">
+      <header className=" border-x md:border bg-background/20 backdrop-blur-lg supports-backdrop-filter:bg-background/20  rounded-full w-full">
+        <div className="mx-auto flex h-16 items-center justify-between gap-3 px-3 sm:px-5">
+          <Link
+            to="/app/dashboard"
+            className="flex shrink-0 items-center gap-2"
+          >
+            <div className="flex items-center gap-1">
+              {/* Logo */}
 
               <img
-                src="/light-logo.png"
+                src={appImgSources[theme as 'light' | 'dark'].logo}
                 alt="Logo"
                 width={40}
                 height={40}
@@ -30,17 +32,9 @@ export default function AppHeader() {
                 decoding="async"
                 className="h-10 w-10 object-contain"
               />
-            </picture>
-
-            {/* App Name */}
-            <picture>
-              <source
-                srcSet="/dark-app-name.png"
-                media="(prefers-color-scheme: dark)"
-              />
 
               <img
-                src="/light-app-name.png"
+                src={appImgSources[theme as 'light' | 'dark'].name}
                 alt="App Name"
                 width={56}
                 height={20}
@@ -48,43 +42,45 @@ export default function AppHeader() {
                 decoding="async"
                 className="mt-2 h-5 w-14 self-end object-contain"
               />
-            </picture>
-          </div>
-        </Link>
-
-        {/* Right Actions */}
-        <div className="flex items-center gap-2 sm:gap-3">
-          <Button variant={'outline'} size={'icon-lg'}>
-            <Bell className="size-5" />
-          </Button>
-
-          <ModeToggle />
-
-          {/* Profile */}
-          <Link
-            to="/app/profile"
-            className="hidden items-center gap-3 rounded-full bg-background/40 p-1.5 pe-4 transition-all hover:bg-accent md:flex"
-          >
-            <Avatar className="size-11">
-              <AvatarFallback className="bg-primary/10 text-sm font-semibold">
-                {isPending ? '..' : data?.user.name?.slice(0, 2).toUpperCase()}
-              </AvatarFallback>
-            </Avatar>
-
-            {!isPending && (
-              <div className="max-w-40 overflow-hidden">
-                <h6 className="truncate text-sm font-semibold">
-                  {data?.user.name}
-                </h6>
-
-                <p className="truncate text-xs text-muted-foreground">
-                  {data?.user.email}
-                </p>
-              </div>
-            )}
+            </div>
           </Link>
+
+          {/* Right Actions */}
+          <div className="flex items-center gap-2 sm:gap-3">
+            <Button variant={'outline'} size={'icon-lg'}>
+              <Bell className="size-5" />
+            </Button>
+
+            <ModeToggle />
+
+            {/* Profile */}
+            <Link
+              to="/app/profile"
+              className="hidden items-center gap-3 rounded-full bg-background/40 p-1.5 transition-all hover:bg-accent md:flex"
+            >
+              <Avatar className="size-11">
+                <AvatarFallback className="bg-primary/10 text-sm font-semibold">
+                  {isPending
+                    ? '..'
+                    : data?.user.name?.slice(0, 2).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+
+              {!isPending && (
+                <div className="max-w-40 overflow-hidden">
+                  <h6 className="truncate text-sm font-semibold">
+                    {data?.user.name}
+                  </h6>
+
+                  <p className="truncate text-xs text-muted-foreground">
+                    {data?.user.email}
+                  </p>
+                </div>
+              )}
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
---
+++ packages/client/src/constants/app-image-sources.ts
@@ -0,0 +1,10 @@
+export const appImgSources = {
+  light: {
+    logo: '/dark-logo.png',
+    name: '/dark-app-name.png',
+  },
+  dark: {
+    logo: '/light-logo.png',
+    name: '/light-app-name.png',
+  },
+} as const;
